5886c5c85499f59f0a049b0723a10b5b23794df3
Thomas Yoon <thomas081891@hotmail.com>
2014-11-03 22:02:41 +0000

    game logic working

--- jsbet.js
+++ jsbet.js
@@ -1,6 +1,24 @@
+$(document).ready(function(){
+
 // Global Variables //
-var startingCash = 100;
+  var randNum = Math.floor((Math.random() * 10) + 1);
+  var startingCash = 100;
 
+// Prompt Variables //
+  var numBet = prompt("Enter a bet between $5-$10!", "Enter your bet!");
+  var numPrompt = prompt("Enter a number between 1-10!", "Pick a number!");
 
-// Function Variables //
-var inputBet = prompt("Please enter a bet between $5-10!", 5);
+  var gameNum = randNum;
+    if (numPrompt == gameNum + 1 || numPrompt == gameNum - 1) {
+      alert("You were one number off. Your current money is" + " " + startingCash + ".");
+      startingCash == startingCash;
+    }
+      else if (numPrompt == gameNum) {
+        startingCash == (startingCash += (numBet * 2));
+          alert("You got it! Your current money is" + " " + startingCash + ".");
+      } 
+        else {
+          alert("Sorry, that was the wrong number. Your current money is" + " " + startingCash + ".")
+          startingCash == (startingCash -= numBet);
+        }
+});
